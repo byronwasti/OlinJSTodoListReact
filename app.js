328a64e65af38bc7850a22bcb32483ead4dc8ed1
byronwasti <byron.wasti@gmail.com>
2016-02-23 16:06:10 -0500

    heroku integration

--- app.js
+++ app.js
@@ -35,4 +35,4 @@ app.post('/api/removeTasks', index.removeTasks);
 app.post('/api/completeTasks', index.completeTasks);
 app.post('/api/editTasks', index.editTasks);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
